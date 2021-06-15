1d1f9979a06c24b8250ac2351249e91f1f61e5b9
uichi <37263474+uichi@users.noreply.github.com>
2021-06-15 19:35:06 +0900

    Add support_animal and service in Hospital table.

--- animal_organization/admin.py
+++ animal_organization/admin.py
@@ -3,17 +3,17 @@ from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
 
-from animal_organization.models import Hospital
+from .models import Hospital
 
 class HospitalResource(resources.ModelResource):
 
   class Meta:
     model = Hospital
-    fields = ('id', 'name', 'address', 'phone_number', )
+    fields = ('id', 'name', 'address', 'phone_number', 'support_animal', 'service', )
     import_id_fields = ('id', )
 
 class HospitalAdmin(ImportExportModelAdmin):
-  list_display = ('name', 'address', 'phone_number', )
+  list_display = ('name', 'address', 'phone_number', 'support_animal', 'service', )
   resource_class = HospitalResource
 
 admin.site.register(Hospital, HospitalAdmin)
--- animal_organization/models.py
+++ animal_organization/models.py
@@ -5,6 +5,8 @@ class Hospital(models.Model):
     name = models.CharField(max_length=128, blank=False, null=False)
     address = models.CharField(max_length=256, blank=False, null=True)
     phone_number = models.CharField(max_length=16, blank=False, null=True)
+    support_animal = models.CharField(max_length=128, blank=False, null=True)
+    service = models.CharField(max_length=128, blank=False, null=True)
     home_page = models.CharField(max_length=256, blank=False, null=True)
 
     def __str__(self):
