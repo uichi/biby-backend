21a2de9af7bcd58a2bd5340e2d533aa2281d89e1
uichi <37263474+uichi@users.noreply.github.com>
2021-06-15 20:11:32 +0900

    Develop multiple keyword search in red list.

--- red_list/views.py
+++ red_list/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
 from .models import Animal, Category, Classification
-from django.db.models import Q, query
+from django.db.models import Q
+from functools import reduce
+import operator
 
 
 class Top(ListView):
@@ -13,10 +15,14 @@ class Top(ListView):
         query = self.request.GET.get('query')
 
         if query:
-            category = Category.objects.filter(name__contains=query)
-            classification = Classification.objects.filter(name__contains=query)
+            category = Category.objects.filter(reduce(operator.or_, (Q(name__contains=keyword) for keyword in query.split())))
+            classification = Classification.objects.filter(reduce(operator.or_, (Q(name__contains=keyword) for keyword in query.split())))
             object_list = Animal.objects.filter(
-                Q(japanese_name__contains=query) | Q(scientific_name__contains=query) | Q(category__in=category) | Q(classification__in=classification))
+                reduce(operator.or_, (Q(japanese_name__contains=keyword) for keyword in query.split())) |
+                reduce(operator.or_, (Q(scientific_name__contains=keyword) for keyword in query.split())) |
+                Q(category__in=category)|
+                Q(classification__in=classification)
+                )
         else:
             object_list = Animal.objects.all()
         return object_list
