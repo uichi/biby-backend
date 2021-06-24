5a2d8a9101b1be7feea2bede5d9c8cfc9da1ffc0
uichi <37263474+uichi@users.noreply.github.com>
2021-06-24 17:40:30 +0900

    Add corsheaders to settings.py.

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'import_export',
     'rest_framework',
+    'corsheaders',
     'imagekit',
     'django_cleanup',
     'red_list',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -140,3 +142,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+CORS_ALLOWED_ORIGINS = ['localhost:3000']
